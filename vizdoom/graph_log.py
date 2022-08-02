a7f974dc38ae8982f684bf7c9bff3b5d79e0ee44
Hugh Perkins <hughperkins@Hugh-Personal.lan>
2022-08-02 08:56:29 -0400

    re-add averaging to graph

--- vizdoom/graph_log.py
+++ vizdoom/graph_log.py
@@ -20,20 +20,24 @@ def run(args):
 		print('available values', log_rows[0].keys())
 		return
 	values = [row[args.y_axis] for row in log_rows]
-	plt.plot(episodes, values)
+	# plt.plot(episodes, values)
 	# plt.savefig('vizdoom/graph.png')
-	# episodes_avg = []
-	# losses_avg = []
-	# average_over = 50
-	# num_batches = len(episodes) // average_over
-	# for b in range(num_batches):
-	# 	b_start = b * average_over
-	# 	b_end = b_start + average_over
-	# 	avg_episode = sum(episodes[b_start:b_end]) / average_over
-	# 	avg_loss = sum(losses[b_start:b_end]) / average_over
-	# 	episodes_avg.append(avg_episode)
-	# 	losses_avg.append(avg_loss)
-	# plt.plot(episodes_avg, losses_avg)
+	if args.average_over == 1:
+		plt.plot(episodes, values)
+	else:
+		episodes_avg = []
+		values_avg = []
+		# average_over = 50
+		num_batches = len(episodes) // args.average_over
+		for b in range(num_batches):
+			b_start = b * args.average_over
+			b_end = b_start + args.average_over
+			avg_episode = sum(episodes[b_start:b_end]) / args.average_over
+			_values = [float(v) for v in values[b_start:b_end]]
+			avg_value = sum(_values) / args.average_over
+			episodes_avg.append(avg_episode)
+			values_avg.append(avg_value)
+		plt.plot(episodes_avg, values_avg)
 	plt.xlabel('batch')
 	plt.ylabel(args.y_axis)
 	image_filepath = args.out_imagefile.format(y_axis=args.y_axis)
@@ -51,5 +55,6 @@ if __name__ == '__main__':
 	parser.add_argument('--out-imagefile', type=str, default='tmp/graph_{y_axis}.png')
 	parser.add_argument('--max-batch', type=int)
 	parser.add_argument('--y-axis', default='reward')
+	parser.add_argument('--average-over', type=int, default=1)
 	args = parser.parse_args()
 	run(args)
